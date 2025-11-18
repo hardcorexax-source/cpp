9af4be33e5779c6c06fd16fc31679c7dea0cf315
hardcorexax-source <hardcorexax@gmail.com>
2025-11-18 15:49:26 +0200

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class figure() {
+    double x;
+    double y;
+  public:
+    figure() : x(0), y(0) {}
+    figure() : x(x), y(y) {}
+    virtual double perimetr();
+    virtual double area();
+};
+
+class round : public figure {
+    double perimetr() {
+        return x*y*3.14;
+    }
+};
+
+class round : public figure {
+    round() : figure(0,0) {}
+    round(x,y) : figure(x,y) {}
+    double perimetr() {
+        return x/2*3.14;
+    }
+    double area() {
+        return x/2*y/2*3.14;
+    }
+};
+
+class quadr : public figure {
+    double perimetr() {
+        return 2*(x+y);
+    }
+    double area() {
+        return x*y;
+    }
+};
+
+int main() {
+    
+    vector<figure> arr;
+
+    arr.push_back(quadr(2,2));
+    arr.push_back(round(2,2));
+
+    return 0;
+}
