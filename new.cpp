26d6507273f61d0e9a62be861e36ab0870ff69a7
hardcorexax-source <hardcorexax@gmail.com>
2025-11-29 00:28:02 +0200

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
     cout << "hello" << endl;
+    for (int i =0; i < 10; ++i) {
+        cout << i << " ";
+    }
+
+    cout << endl;
 
 
     return 0;
