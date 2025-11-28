949d3980e2c3968f4aa4c42e1ca42c7a57104abb
hardcorexax-source <hardcorexax@gmail.com>
2025-11-29 00:32:41 +0200

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,32 +1,60 @@
 #include <iostream>
-#include <map>
-#include <functional>
-
-// Универсальный кеш для чистых функций f(int) -> int
-class Memo {
-public:
-    explicit Memo(std::function<long long(int)> func) : f(func) {}
-
-    long long operator()(int n) {
-        if (cache.count(n)) return cache[n];
-        long long result = f(n);
-        cache[n] = result;
-        return result;
-    }
-
-private:
-    std::function<long long(int)> f;
-    std::map<int, long long> cache;
-};
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main() {
-    // Оборачиваем лямбда-функцию в мемоизацию
-    Memo fib([&](int n) -> long long {
-        if (n <= 1) return n;
-        return fib(n-1) + fib(n-2);
-    });
-
-    for (int i = 0; i <= 40; i++) {
-        std::cout << "fib(" << i << ") = " << fib(i) << "\n";
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "socket failed\n";
+        return 1;
     }
+
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    if (bind(server_fd, (sockaddr*)&address, sizeof(address)) < 0) {
+        std::cerr << "bind failed\n";
+        return 1;
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        std::cerr << "listen failed\n";
+        return 1;
+    }
+
+    std::cout << "HTTP server started on http://localhost:8080\n";
+
+    while (true) {
+        socklen_t addrlen = sizeof(address);
+        int client_fd = accept(server_fd, (sockaddr*)&address, &addrlen);
+        if (client_fd < 0) {
+            std::cerr << "accept failed\n";
+            continue;
+        }
+
+        char buffer[2048] = {0};
+        read(client_fd, buffer, sizeof(buffer));
+
+        std::cout << "Request:\n" << buffer << "\n";
+
+        std::string body = "<h1>Hello from C++ HTTP Server</h1>";
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: " + std::to_string(body.size()) + "\r\n"
+            "Connection: close\r\n"
+            "\r\n" +
+            body;
+
+        send(client_fd, response.c_str(), response.size(), 0);
+        close(client_fd);
+    }
+
+    close(server_fd);
+    return 0;
 }
